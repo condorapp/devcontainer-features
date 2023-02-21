d2ddaa04c56d71d70b3d3ccd0953dc810a69b4c7
Tres Trantham <tres@trestrantham.com>
2023-02-20 21:45:15 -0600

    initial version 1.0.4

--- src/asdf/install.sh
+++ src/asdf/install.sh
@@ -6,24 +6,13 @@ echo "Installing plugins and packages with ASDF"
 PLUGINS=${PLUGINS:-undefined}
 echo "Provided plugin(s): $PLUGINS"
 
-# The 'install.sh' entrypoint script is always executed as the root user.
-#
-# These following environment variables are passed in by the dev container CLI.
-# These may be useful in instances where the context of the final 
-# remoteUser or containerUser is useful.
-# For more details, see https://containers.dev/implementors/features#user-env-var
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
-
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
-
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-echo ". ~/.asdf/asdf.sh" >> ~/.zshrc
+su -c "git clone https://github.com/asdf-vm/asdf.git ~/.asdf" - $_CONTAINER_USER
+su -c "echo \". ~/.asdf/asdf.sh\" >> ~/.zshrc" - $_CONTAINER_USER
+su -c "source ~/.zshrc" - $_CONTAINER_USER
 
 for PLUGIN in $(echo $PLUGINS | sed "s/,/ /g")
 do
-    asdf plugin add $PLUGIN
+    su -c "asdf plugin add $PLUGIN" - $_CONTAINER_USER
 done
 
-asdf install
+su -c "asdf install" - $_CONTAINER_USER
