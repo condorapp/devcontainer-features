34fd43c921154aa3f6d53ab94122378f3e858b7b
Tres Trantham <tres@trestrantham.com>
2023-02-21 04:23:16 +0000

    force zsh

--- src/asdf/install.sh
+++ src/asdf/install.sh
@@ -7,13 +7,13 @@ echo "Installing plugins and packages with ASDF"
 PLUGINS=${PLUGINS:-undefined}
 echo "Provided plugin(s): $PLUGINS"
 
-su -c "git clone https://github.com/asdf-vm/asdf.git ~/.asdf" - $_CONTAINER_USER
-su -c "echo \". ~/.asdf/asdf.sh\" >> ~/.zshrc" - $_CONTAINER_USER
-su -c ". ~/.zshrc" - $_CONTAINER_USER
+su -s /bin/zsh -c "git clone https://github.com/asdf-vm/asdf.git ~/.asdf" - $_CONTAINER_USER
+su -s /bin/zsh -c "echo \". ~/.asdf/asdf.sh\" >> ~/.zshrc" - $_CONTAINER_USER
+su -s /bin/zsh -c "source ~/.zshrc" - $_CONTAINER_USER
 
 for PLUGIN in $(echo $PLUGINS | sed "s/,/ /g")
 do
-    su -c "asdf plugin add $PLUGIN" - $_CONTAINER_USER
+    su -s /bin/zsh -c "asdf plugin add $PLUGIN" - $_CONTAINER_USER
 done
 
-su -c "asdf install" - $_CONTAINER_USER
+su -s /bin/zsh -c "asdf install" - $_CONTAINER_USER
