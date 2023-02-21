12f59717b0938ac961a6bd99bf0c2029f7bbc774
Tres Trantham <tres@trestrantham.com>
2023-02-21 04:31:05 +0000

    s

--- src/asdf/install.sh
+++ src/asdf/install.sh
@@ -11,9 +11,11 @@ su -s /bin/zsh -c "git clone https://github.com/asdf-vm/asdf.git ~/.asdf" - $_CO
 su -s /bin/zsh -c "echo \". ~/.asdf/asdf.sh\" >> ~/.zshrc" - $_CONTAINER_USER
 su -s /bin/zsh -c "source ~/.zshrc" - $_CONTAINER_USER
 
+su -s /bin/zsh -c "cat ~/.zshrc" - $_CONTAINER_USER
+
 for PLUGIN in $(echo $PLUGINS | sed "s/,/ /g")
 do
-    su -s /bin/zsh -c "asdf plugin add $PLUGIN" - $_CONTAINER_USER
+    su -s /bin/zsh -c "source ~/.zshrc; asdf plugin add $PLUGIN" - $_CONTAINER_USER
 done
 
 su -s /bin/zsh -c "asdf install" - $_CONTAINER_USER
