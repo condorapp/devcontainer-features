321c5f004943ea2f1822c6a65511a603387d85dd
Tres Trantham <tres@trestrantham.com>
2023-02-21 04:31:05 +0000

    bugfix

--- src/asdf/install.sh
+++ src/asdf/install.sh
@@ -9,11 +9,10 @@ echo "Provided plugin(s): $PLUGINS"
 
 su -s /bin/zsh -c "git clone https://github.com/asdf-vm/asdf.git ~/.asdf" - $_CONTAINER_USER
 su -s /bin/zsh -c "echo \". ~/.asdf/asdf.sh\" >> ~/.zshrc" - $_CONTAINER_USER
-su -s /bin/zsh -c "source ~/.zshrc" - $_CONTAINER_USER
 
 for PLUGIN in $(echo $PLUGINS | sed "s/,/ /g")
 do
-    su -s /bin/zsh -c "asdf plugin add $PLUGIN" - $_CONTAINER_USER
+    su -s /bin/zsh -c ". ~/.zshrc; asdf plugin add $PLUGIN" - $_CONTAINER_USER
 done
 
-su -s /bin/zsh -c "asdf install" - $_CONTAINER_USER
+su -s /bin/zsh -c ". ~/.zshrc; asdf install" - $_CONTAINER_USER
